fe7b73a1676cf60bd03a18ba6009c0daa822037b
CY252186 <74874755+CY252186@users.noreply.github.com>
2021-11-06 17:19:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(1200,400);
+  this.snow3.jpg = loadImage("sprites/snow3.jpg");
+}
+  
+
+function draw() {
+  background(255,255,255); 
+   snow3.jpg.diplay();
+  drawSprites();
+}
+
